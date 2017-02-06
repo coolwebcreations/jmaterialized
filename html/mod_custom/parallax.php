2a2a4ef56dd92d4fbc539cca309e385c2312b9b2
Mike Brandner <mike@coolwebcreations.de>
2017-02-06 12:21:18 +0100

    Create parallax.php

---
+++ html/mod_custom/parallax.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<div class="parallax">
+  <img class="responsive-img" src="<?php echo $params->get('backgroundimage');?>">
+</div>
+<div class="<?php echo $moduleclass_sfx ?>">
+	<?php echo $module->content;?>
+</div>
