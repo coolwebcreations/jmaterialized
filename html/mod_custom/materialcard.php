e999f4b3fe554c3568f8f3c86954774e94469260
Mike Brandner <mike@coolwebcreations.de>
2017-02-06 12:21:02 +0100

    Create materialcard.php

---
+++ html/mod_custom/materialcard.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+          <div class="<?php echo $moduleclass_sfx ?>">
+            <?php if ($params->get('backgroundimage')) : ?>
+            <div class="card-image">
+              <img src="<?php echo $params->get('backgroundimage');?>">
+              <span class="card-title"><?php echo $module->title;?></span>
+            </div>
+            <?php endif;?>
+            <?php if ($module->showtitle) : ?>
+            	<span class="card-title">
+                <h2><?php echo $module->title;?></h2></span>
+            <?php endif;?>
+            <div class="card-content">
+              <?php echo $module->content;?>
+            </div>
+          </div>
