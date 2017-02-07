15e1bf99fb920a029a212c50fb22e113a386227b
Mike Brandner <mike@coolwebcreations.de>
2017-02-07 15:31:48 +0100

    Update default.php

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -110,6 +110,46 @@ if (($tagId = $params->get('tag_id', '')))
 	}
 }
 ?></ul>
+	
+<ul class="side-nav" id="mobile-menu">	
+<?php foreach ($list as $i => &$item)
+{
+	$class = 'item-' . $item->id;
+
+	if ($item->id == $default_id)
+	{
+		$class .= ' default';
+	}
+
+
+	if (($item->id == $active_id) || ($item->type == 'alias' && $item->params->get('aliasoptions') == $active_id))
+	{
+		$class .= ' current';
+	}
+
+	if (in_array($item->id, $path))
+	{
+		$class .= ' active';
+	}
+	elseif ($item->type == 'alias')
+	{
+		$aliasToId = $item->params->get('aliasoptions');
+
+		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
+		{
+			$class .= ' active';
+		}
+		elseif (in_array($aliasToId, $path))
+		{
+			$class .= ' alias-parent-active';
+		}
+	}
+
+	if ($item->type == 'separator')
+	{
+		$class .= ' divider';
+	}
+	?></ul>
   </div>
   </div>
 </nav>
