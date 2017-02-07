50ac93210f149553402a45f4eea6eb2c48655cc1
Mike Brandner <mike@coolwebcreations.de>
2017-02-07 15:27:02 +0100

    Update default.php

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -5,10 +5,13 @@
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * Template JMaterialized by cool Webcreations
+ * JMaterialized Template by cool Webcreations / coolwebcreations.de 
  */
 
 defined('_JEXEC') or die;
+
+// Getting params from template
+$params = $app->getTemplate(true)->params;
 
 $id = '';
 
@@ -19,12 +22,11 @@ if (($tagId = $params->get('tag_id', '')))
 
 // The menu class is deprecated. Use nav instead
 ?>
-
-<nav class="">
+<nav class="<?php echo $params->get('nav_suffix'); ?>">
   
 <div class="container ">
 <div class="nav-wrapper">
-<a id="logo-container" href="<?php echo $this->baseurl; ?>" class="brand-logo"><img style="height: 65px; padding: 5px;" src="" alt="cool Webcreations"></a>
+<a id="logo-container" href="<?php echo JURI::base(); ?>" class="brand-logo"><img style="height: 65px; padding: 5px;" src="<?php echo JURI::base() . $params->get('logo_url'); ?>" alt="cool Webcreations"></a>
 					<a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 <ul id="nav-mobile" class="<?php echo $class_sfx ?>" style="transform: translateX(0%);">
 <?php foreach ($list as $i => &$item)
@@ -108,90 +110,6 @@ if (($tagId = $params->get('tag_id', '')))
 	}
 }
 ?></ul>
-  
   </div>
   </div>
 </nav>
-  
-  <ul class="side-nav" id="mobile-menu" style="transform: translateX(0px);">
-  <?php foreach ($list as $i => &$item)
-{
-	$class = 'item-' . $item->id;
-
-	if ($item->id == $default_id)
-	{
-		$class .= ' default';
-	}
-
-
-	if (($item->id == $active_id) || ($item->type == 'alias' && $item->params->get('aliasoptions') == $active_id))
-	{
-		$class .= ' current';
-	}
-
-	if (in_array($item->id, $path))
-	{
-		$class .= ' active';
-	}
-	elseif ($item->type == 'alias')
-	{
-		$aliasToId = $item->params->get('aliasoptions');
-
-		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
-		{
-			$class .= ' active';
-		}
-		elseif (in_array($aliasToId, $path))
-		{
-			$class .= ' alias-parent-active';
-		}
-	}
-
-	if ($item->type == 'separator')
-	{
-		$class .= ' divider';
-	}
-
-	if ($item->deeper)
-	{
-		$class .= ' deeper';
-	}
-
-	if ($item->parent)
-	{
-		$class .= ' parent';
-	}
-
-	echo '<li class="' . $class . '">';
-
-	switch ($item->type) :
-		case 'separator':
-		case 'component':
-		case 'heading':
-		case 'url':
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_' . $item->type);
-			break;
-
-		default:
-			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
-			break;
-	endswitch;
-
-	// The next item is deeper.
-	if ($item->deeper)
-	{
-		echo '<ul class="dropdown-content">';
-	}
-	// The next item is shallower.
-	elseif ($item->shallower)
-	{
-		echo '</li>';
-		echo str_repeat('</ul></li>', $item->level_diff);
-	}
-	// The next item is on the same level.
-	else
-	{
-		echo '</li>';
-	}
-}
-?></ul>
